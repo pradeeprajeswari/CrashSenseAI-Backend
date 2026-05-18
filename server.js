529cc7c2a513d103cbff506a64ae22b8612c3e79
Pradeep <pradeeprajeswari720@gmail.com>
2026-05-18 08:37:43 +0530

    Final Socket.io deployment fix

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const io = new Server(server, {
     }
 });
 
+
 app.use(cors());
 app.use(express.json());
 
@@ -135,6 +136,8 @@ app.put("/accept/:id", async (req, res) => {
 
 });
 
-server.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
